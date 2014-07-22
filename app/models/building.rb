629bd2faa8a2cc75480210ec18b1b8564908272a
hassanjalilk <hjalil.hba2014@ivey.ca>
2014-07-22 18:42:59 -0400

    did show page of the building

--- app/models/building.rb
+++ app/models/building.rb
@@ -3,4 +3,13 @@ class Building < ActiveRecord::Base
 	has_many :issues, through: :tenants
 	belongs_to :company
 	belongs_to :manager
+
+	# def avg_time_for_complain_to_complete
+	# 	self.tenants.each do |tenant|
+	# 		tenant.each do|issue|
+	# 			issue.completed_time - issue.created_time
+	# 		end
+	# 	end
+	# end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+
   resources :buildings
 
   # The priority is based upon order of creation: first created -> highest priority.
