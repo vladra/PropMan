814cf6fbb985d4c15641b702ea045fbdcb6d6551
hassanjalilk <hjalil.hba2014@ivey.ca>
2014-07-23 12:34:02 -0400

    added methods in comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,27 @@
 class CommentsController < ApplicationController
+
   def new
+  	@comment = Comment.new
   end
 
   def create
+  	@comment = Comment.new(comment_params)
+  	if @comment.save
+  		redirect_to issue_path
+  	else
+  		render :new
   end
 
   def destroy
+  	@comment = @comment.find(params[:id])
+  	@comment.delete
+  	redirect_to issue_path
   end
+
+  private
+
+  def comment_params
+  	params.require(:commet).permit(:message, :user_id, :issue_id)
+  end
+
 end
