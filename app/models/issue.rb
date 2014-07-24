9649c74c5b9ed5cbe5d34e4927dba06f1e4ddcea
Vladimir Dralo <svolard@gmail.com>
2014-07-24 17:49:20 -0400

    Issue model, duration method

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -2,4 +2,13 @@ class Issue < ActiveRecord::Base
 	has_many :comments
 	belongs_to :tenant
 	has_and_belongs_to_many :categories
+
+	def duration
+		if complete_date.nil?
+			(Time.now - created_at).floor
+		else
+			(complete_date - created_at).floor
+		end
+	end
+
 end
