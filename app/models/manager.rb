430e85006e41da02a2294dc006be06e3b8aa37cd
hassanjalilk <hjalil.hba2014@ivey.ca>
2014-07-24 19:44:39 -0400

    added a profile page for managers

--- app/models/manager.rb
+++ app/models/manager.rb
@@ -7,4 +7,8 @@ class Manager < ActiveRecord::Base
 	has_many :companies, through: :buildings
 	has_many :issues, through: :buildings
 	has_many :comments, as: :commentable
+
+	def no_of_new_issues
+		issues.where(created_at: (Date.current.beginning_of_day..Date.current.end_of_day)).count
+	end
 end
