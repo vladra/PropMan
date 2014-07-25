2d12e9e6cf97139a58ed27128e59ceaf4ad9577b
Vladimir Dralo <svolard@gmail.com>
2014-07-24 22:30:38 -0400

    Setting action for Tenant and links for this action

--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -1,5 +1,12 @@
 class TenantsController < ApplicationController
+	before_action :authenticate_tenant!
+
   def show
   	@tenant = current_tenant
   end
+
+  def settings
+  	@tenant = current_tenant
+  end
+
 end
