f6a743b137673a14a7157b0b81e441864e2ddba1
Vladimir Dralo <svolard@gmail.com>
2014-07-25 00:08:26 -0400

    Tenant controller and views changes for sending request for new building

--- app/controllers/tenants_controller.rb
+++ app/controllers/tenants_controller.rb
@@ -16,13 +16,23 @@ class TenantsController < ApplicationController
   end
 
   def change_building
+  	@tenant = current_tenant
+  end
 
+  def update_building
+  	@tenant = current_tenant
+  	@tenant.update(building_params)
+  	redirect_to tenant_path, notice: 'Request successfully sent! Aproval can take some times, please check later..'
   end
 
 private
 
 	def tenant_params
 		params.require(:tenant).permit(:first_name, :last_name, :phone_number)
+	end
+
+	def building_params
+		params.require(:tenant).permit(:building_id, :apartment)
 	end
 
 end
