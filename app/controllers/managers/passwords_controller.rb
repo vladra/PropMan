d672684f2c11a9dc4fb1ccd024a3783a53b89dd9
hassanjalilk <hjalil.hba2014@ivey.ca>
2014-07-25 19:25:31 -0400

    Changes to managers controller and devise controllers for managers

---
+++ app/controllers/managers/passwords_controller.rb
@@ -0,0 +1,9 @@
+class Managers::PasswordsController < Devise::PasswordsController
+
+protected
+
+  # def after_resetting_password_path_for(resource)
+  #   signed_in_root_path(resource)
+  # end
+
+end
