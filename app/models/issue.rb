4878f6dad2364490bec60a9ad24023cc2d36ba61
Vladimir Dralo <svolard@gmail.com>
2014-07-26 12:50:46 -0400

    Issue model. Statuses and before save callbacks related to them

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -3,7 +3,20 @@ class Issue < ActiveRecord::Base
 	belongs_to :tenant
 	has_and_belongs_to_many :categories
 
-	statuses = [:done]
+	before_validation :set_status_new, on: [:create]
+	before_validation :check_if_done, on: [:update]
+
+	def set_status_new
+		self.status = :new
+	end
+
+	def check_if_done
+		if self.status = 'done'
+			self.complete_date = Time.now
+		else
+			self.complete_date = nil
+		end
+	end
 
 	def duration
 		if complete_date.nil?
@@ -11,6 +24,10 @@ class Issue < ActiveRecord::Base
 		else
 			(complete_date - created_at).floor
 		end
+	end
+
+	def self.statuses
+		[:new, :pending, :done]
 	end
 
 end
