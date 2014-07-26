64c41fe47aa39175fc357d10a2acf2da337e3ad6
Vladimir Dralo <svolard@gmail.com>
2014-07-26 11:59:53 -0400

    Tenants controller done

--- app/controllers/tenants/registrations_controller.rb
+++ app/controllers/tenants/registrations_controller.rb
@@ -3,11 +3,11 @@ class Tenants::RegistrationsController < Devise::RegistrationsController
 protected
 
   def after_sign_up_path_for(resource)
-    tenant_path
+    tenants_path
   end
 
   def after_update_path_for(resource)
-    tenant_path
+    tenants_path
   end
 
 end
