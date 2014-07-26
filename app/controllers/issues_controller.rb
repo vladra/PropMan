178bb0d3e7f094cfcad44f1a20fc788ba875c3e0
Vladimir Dralo <svolard@gmail.com>
2014-07-26 12:00:24 -0400

    Issues controller changes

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,25 +1,26 @@
 class IssuesController < ApplicationController
-  # before_action :authenticate_user!, only: [:create, :edit, :update]
+  before_action :authenticate_tenant!, only: [:create, :edit, :update]
 
   def index
     if current_tenant
-      @issues = Issue.where(tenant: current_tenant)
+      @issues = current_tenant.issues.order('created_at desc')
     elsif current_manager
-
+      @issues = current_manager.issues.order('created_at desc')
     else
-      redirect_to 'root', flash: "BS"
+      redirect_to root_path
     end
   end
 
   def show
     @issue = Issue.find(params[:id])
+    @comments = @issue.comments.order('created_at desc')
   end
 
   def create
   	@issue = Issue.new(issue_params)
     @issue.tenant = current_tenant
   	if @issue.save
-  		redirect_to tenant_path
+  		redirect_to tenants_path, notice: "Issue successfully added!"
   	else
   		render :new
   	end
