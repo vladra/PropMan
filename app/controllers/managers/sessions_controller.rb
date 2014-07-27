78bd6c929137c72a848846cb292b15701e9dfd68
Vladimir Dralo <svolard@gmail.com>
2014-07-26 21:28:14 -0400

    Changes to devise sessions controllers to allow only one session at a a time (no current_tenant and current_manager at the same time anymore)

--- app/controllers/managers/sessions_controller.rb
+++ app/controllers/managers/sessions_controller.rb
@@ -1,5 +1,12 @@
 class Managers::SessionsController < Devise::SessionsController
 
+	def create
+		super
+		if current_tenant
+			sign_out current_tenant
+		end
+	end
+
 protected
 
 	def after_sign_in_path_for(resource)
--- app/controllers/tenants/sessions_controller.rb
+++ app/controllers/tenants/sessions_controller.rb
@@ -1,5 +1,12 @@
 class Tenants::SessionsController < Devise::SessionsController
 
+	def create
+		super
+		if current_manager
+			sign_out current_manager
+		end
+	end
+
 protected
 
 	def after_sign_in_path_for(resource)
