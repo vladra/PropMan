2d2edfda31068d731088c39d4e2de2fb202d8856
Vladimir Dralo <svolard@gmail.com>
2014-07-26 23:32:23 -0400

    Fixes to Building model

--- app/models/building.rb
+++ app/models/building.rb
@@ -4,6 +4,10 @@ class Building < ActiveRecord::Base
 	belongs_to :company
 	belongs_to :manager
 
+	def unresolved_issues
+		issues.where.not(status: 'done')
+	end
+
 	def no_of_new_issues
 		issues.where(created_at: (Date.current.beginning_of_day..Date.current.end_of_day)).count
 	end
@@ -12,11 +16,11 @@ class Building < ActiveRecord::Base
 		issues.where(status: 'done').count
 	end
 
-	def avg_time_for_completion
-		(issues.where(status: 'done').pluck(:complete_date, :created_at).reduce(0) do |total, (complete_date, created_at)|
-			total += complete_date - created_at
-		end / issues.where(status: 'done').count / 3600 / 24).floor
-	end
+	# def avg_time_for_completion
+	# 	(issues.where(status: 'done').pluck(:complete_date, :created_at).reduce(0) do |total, (complete_date, created_at)|
+	# 		total += complete_date - created_at
+	# 	end / issues.where(status: 'done').count / 3600 / 24).floor
+	# end
 
 	def avg_rating
 		('%.2f' % issues.average(:rating).to_f).to_f
