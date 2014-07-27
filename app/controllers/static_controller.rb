9f508f6a956ada214fba82e92d879cace9f8ab75
Vladimir Dralo <svolard@gmail.com>
2014-07-27 17:46:59 -0400

    Redirect from index page if you are logged in

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,7 +1,13 @@
 class StaticController < ApplicationController
 
 	def index
-		render layout: 'landing_page'
+		if current_tenant
+			redirect_to tenants_path
+		elsif current_manager
+			redirect_to managers_path
+		else
+			render layout: 'landing_page'
+		end
 	end
 
 end
