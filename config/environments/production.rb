64de5e9ef7f917a3ca8be0f03fb826ef3fdb7bad
Vladimir Dralo <svolard@gmail.com>
2014-07-28 18:01:22 -0400

    images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ Rails.application.configure do
   # Paperclip and AWS 3
   config.paperclip_defaults = {
     :storage => :s3,
-    # :s3_host_name => "propman-production.s3.amazonaws.com",
+    :s3_host_name => "s3-us-west-2.amazonaws.com",
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
