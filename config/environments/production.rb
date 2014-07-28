9f5d2294e5dfe053af872d0808e4a4ffe82b006a
Vladimir Dralo <svolard@gmail.com>
2014-07-28 17:48:36 -0400

    Fixes to make paperclip work with aws

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,12 +19,14 @@ Rails.application.configure do
   # Paperclip and AWS 3
   config.paperclip_defaults = {
     :storage => :s3,
+    :s3_host_name => "propman-production.s3.amazonaws.com"
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
+  Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
 
   # Code is not reloaded between requests.
   config.cache_classes = true
