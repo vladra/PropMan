8eee78bb047caebfe3f06fd48fe69860b7e44ea1
hassanjalilk <hjalil.hba2014@ivey.ca>
2014-07-28 16:55:39 -0400

    created categories and sub categories

--- db/schema.rb
+++ db/schema.rb
@@ -75,6 +75,14 @@ ActiveRecord::Schema.define(version: 20140728164714) do
   add_index "managers", ["email"], name: "index_managers_on_email", unique: true, using: :btree
   add_index "managers", ["reset_password_token"], name: "index_managers_on_reset_password_token", unique: true, using: :btree
 
+  create_table "suers", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "login"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "tenants", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
@@ -102,5 +110,13 @@ ActiveRecord::Schema.define(version: 20140728164714) do
 
   add_index "tenants", ["email"], name: "index_tenants_on_email", unique: true, using: :btree
   add_index "tenants", ["reset_password_token"], name: "index_tenants_on_reset_password_token", unique: true, using: :btree
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "login"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
